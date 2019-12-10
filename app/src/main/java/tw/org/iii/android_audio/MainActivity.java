98d508736dd878fef9688ed7a496f9e31f1362ea
MayGrass <15877196+MayGrass@users.noreply.github.com>
2019-12-10 13:46:17 +0800

    播放自訂音效

--- app/src/main/java/tw/org/iii/android_audio/MainActivity.java
+++ app/src/main/java/tw/org/iii/android_audio/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private AudioManager audioManager;
-
+    private SoundPool soundPool;
+    int h1,h2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 5);
+        h1 = soundPool.load(this, R.raw.h1, 1);
+        h2 = soundPool.load(this, R.raw.h2, 1);
     }
 
-    public void playSound(View view) {
+    public void playSystemSound(View view) {
         audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
+    }
+
+    public void PlaySound1(View view) {
+        soundPool.play(h1, 1f, 1f, 1, 0, 1);
+    }
+
+    public void PlaySound2(View view) {
+        soundPool.play(h2, 1f, 1f, 1, 0, 1);
     }
 }
