75f8c638e876dfb603eb61785f3df921a59029cc
MayGrass <15877196+MayGrass@users.noreply.github.com>
2019-12-10 14:32:26 +0800

    按按鈕開始錄音以及停止錄音並儲存在指定的路徑

--- app/src/main/java/tw/org/iii/android_audio/MainActivity.java
+++ app/src/main/java/tw/org/iii/android_audio/MainActivity.java
@@ -1,22 +1,60 @@
 package tw.org.iii.android_audio;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
+import android.media.MediaRecorder;
 import android.media.SoundPool;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     private AudioManager audioManager;
     private SoundPool soundPool;
     int h1,h2;
+    private File musicDir, sdroot;
+    private MediaRecorder mediaRecorder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.RECORD_AUDIO,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    1);
+        }
+        else {
+            init();
+        }
+
+    }
+
+    private void init() {
+
+        //儲存路徑
+        musicDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+        sdroot = Environment.getExternalStorageDirectory();
 
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 5);
@@ -35,4 +73,40 @@ public class MainActivity extends AppCompatActivity {
     public void PlaySound2(View view) {
         soundPool.play(h2, 1f, 1f, 1, 0, 1);
     }
+
+    public void StartRecord(View view) {
+//        Intent intent = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
+//        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(musicDir, "DCH.amr")));
+//        startActivityForResult(intent, 777);
+        try {
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            //設定輸出檔案格式
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            //解碼方式
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+            mediaRecorder.setOutputFile(new File(sdroot, "DCH.3gp").getAbsolutePath());
+
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void StopRecord(View view) {
+        if (mediaRecorder != null) {
+            mediaRecorder.stop();
+            mediaRecorder.reset();
+            mediaRecorder.release();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.v("DCH", "OK");
+    }
+
+
 }
